37e757d9accd79d071adda7c248b539b02305710
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-08-15 16:48:16 +0900

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { Link } from 'gatsby'
 import Layout from "../components/layouts"
+import { StaticImage } from "gatsby-plugin-image"
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>ohaaaaaaaaaaaaaaaaa</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://placekitten.com/800/600"
+      />
     </Layout>
 
   )
